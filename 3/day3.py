06377a03e211ea2ba5525fe95b41b82e7bd5a6e9
JanSHecker <jan.hecker00@gmail.com>
2024-12-05 16:43:18 +0100

    line stuff

--- 3/day3.py
+++ 3/day3.py
@@ -3,32 +3,25 @@ import re
 with open("3/input", "r") as file:
     input = file.read()
 
-
-multiplications = re.findall("mul[(][0-9]+[,][0-9]+[)]",input)
-
-sum = 0
 def compute(function):
     parts = re.split("[(,)]", function)
     sum = int(parts[1]) * int(parts[2])
     return sum
 
-
-
-
 #part 1
+multiplications = re.findall("mul[(][0-9]+[,][0-9]+[)]",input)
 
+sum = 0
 
 for multiplication in multiplications:
     sum +=compute(multiplication)
-
 print(sum)
 
-
 #part 2
-
 commands = re.findall("mul[(][0-9]+[,][0-9]+[)]|do[(][)]|don[']t[(][)]", input)
 
 sum2 = 0
+
 multiplication_enabled = True
 for command in commands:
     if "mul" in command:  
@@ -37,8 +30,6 @@ for command in commands:
     elif "don't()" in command: 
         multiplication_enabled = False
     elif "do()" in command:  
-        multiplication_enabled = True
-
-        
+        multiplication_enabled = True       
 print(sum2)
         
