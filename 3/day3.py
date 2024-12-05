44739fae35cc2f2dba3476a70753b44c488659a9
JanSHecker <jan.hecker00@gmail.com>
2024-12-05 16:26:35 +0100

    day 3

---
+++ 3/day3.py
@@ -0,0 +1,44 @@
+import re
+
+with open("3/input", "r") as file:
+    input = file.read()
+
+
+multiplications = re.findall("mul[(][0-9]+[,][0-9]+[)]",input)
+
+sum = 0
+def compute(function):
+    parts = re.split("[(,)]", function)
+    sum = int(parts[1]) * int(parts[2])
+    return sum
+
+
+
+
+#part 1
+
+
+for multiplication in multiplications:
+    sum +=compute(multiplication)
+
+print(sum)
+
+
+#part 2
+
+commands = re.findall("mul[(][0-9]+[,][0-9]+[)]|do[(][)]|don[']t[(][)]", input)
+
+sum2 = 0
+multiplication_enabled = True
+for command in commands:
+    if "mul" in command:  
+        if multiplication_enabled:  
+            sum2 += compute(command)
+    elif "don't()" in command: 
+        multiplication_enabled = False
+    elif "do()" in command:  
+        multiplication_enabled = True
+
+        
+print(sum2)
+        
