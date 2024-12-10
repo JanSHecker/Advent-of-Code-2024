52a65038d24e6ea678282197ce62cf17d91826e9
JanSHecker <jan.hecker00@gmail.com>
2024-12-10 18:49:21 +0100

    day7 p2

--- 7/day7.py
+++ 7/day7.py
@@ -31,3 +31,38 @@ for equation in equations:
 
 
 print("final result: " + str(sum))
+
+#part2
+
+
+def check_equation_2(result, operands):
+    #print(result,operands)
+    multipliable = False
+    addable = False
+    concatenatable = False
+    active_operand = operands[len(operands)-1]
+    
+    if len(operands) == 1:
+        #print(active_operand == result)
+        return active_operand == result
+    if result % active_operand == 0:
+        multipliable = check_equation_2(int(result/active_operand), operands[0:len(operands)-1])
+    if result - active_operand > 0:
+        addable = check_equation_2(result - active_operand, operands[0:len(operands)-1])
+    s_result = str(result)
+    s_operand = str(active_operand)
+    if s_result[len(s_result)- len(s_operand):len(s_result)] == s_operand and len(s_result) > len(s_operand):
+        new_result = int(s_result[0:len(s_result)-len(s_operand)])
+        
+        concatenatable = check_equation_2(new_result, operands[0:len(operands)-1])
+    if addable or multipliable or concatenatable:
+        return True
+    return False
+
+sum_2 = 0
+for equation in equations:
+    if check_equation_2(equation[0],equation[1]):
+        sum_2 += equation[0]
+        #print("approved!")
+
+print("final result part2: " + str(sum_2))
